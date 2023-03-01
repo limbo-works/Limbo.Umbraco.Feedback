370f2d58f70c09b54eb37d3df5275d6ad8170557
Anders Bjerner <abjerner@skybrud.dk>
2023-03-01 16:06:40 +0100

    Updated csproj file to reflect new package name
    + upgraded package to a Razor Class Library

--- src/Limbo.Umbraco.Feedback/Manifests/FeedbackManifestFilter.cs
+++ src/Limbo.Umbraco.Feedback/Manifests/FeedbackManifestFilter.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Reflection;
 using Umbraco.Cms.Core.Manifest;
 
 namespace Limbo.Umbraco.Feedback.Manifests {
@@ -13,6 +12,7 @@ namespace Limbo.Umbraco.Feedback.Manifests {
             // Initialize a new manifest filter for this package
             PackageManifest manifest = new() {
                 PackageName = FeedbackPackage.Name,
+                Version = FeedbackPackage.InformationalVersion,
                 BundleOptions = BundleOptions.Independent,
                 Scripts = new[] {
                     $"/App_Plugins/{FeedbackPackage.Alias}/Scripts/Controllers/ContentApp.js",
@@ -24,16 +24,6 @@ namespace Limbo.Umbraco.Feedback.Manifests {
                     $"/App_Plugins/{FeedbackPackage.Alias}/Styles/Default.css"
                 }
             };
-
-            // The "Version" property isn't available in 9.0.0, but it may be in newer releases, so
-            // we need to use reflection for setting it's value. Ideally this shouldn't fail, but
-            // we might as least add a try/catch to be sure
-            try {
-                PropertyInfo property = manifest.GetType().GetProperty("Version");
-                property?.SetValue(manifest, FeedbackPackage.InformationalVersion);
-            } catch {
-                // We don't really care about the exception
-            }
 
             manifests.Add(manifest);
 
